d454c4194a2ba2a911af5c89f658dbfbe1e99adf
Matt Hackmann <matt@dxprog.com>
2018-10-16 23:19:09 -0700

    api server converted to typescript

--- api-server/index.ts
+++ api-server/index.ts
@@ -1,7 +1,12 @@
-const http = require('http');
-const express = require('express');
-const { server: WebSocketServer } = require('websocket');
+import * as http from 'http';
+import * as express from 'express';
+import {
+  server as WebSocketServer,
+  request,
+  connection
+} from 'websocket';
 
+// TODO: Old style import until the whole project is in TS
 const config = require('../config');
 
 const app = express();
@@ -12,16 +17,16 @@ const socketServer = new WebSocketServer({
   autoAcceptConnections: false
 });
 
-let deviceConnection;
+let deviceConnection: connection | null;
 
-function sendMessage(message) {
+function sendMessage(message: any) {
   if (deviceConnection) {
     console.log('Sending message to device', message);
     deviceConnection.sendUTF(JSON.stringify(message));
   }
 }
 
-app.post('/eyes', (req, res) => {
+app.post('/eyes', (req: express.Request, res: express.Response) => {
   if (deviceConnection) {
     sendMessage({
       command: 'EYES',
@@ -33,7 +38,7 @@ app.post('/eyes', (req, res) => {
   res.send('OK');
 });
 
-app.post('/speak', (req, res) => {
+app.post('/speak', (req: express.Request, res: express.Response) => {
   if (deviceConnection) {
     sendMessage({
       command: 'SPEAK',
@@ -47,12 +52,12 @@ httpServer.listen(config.port, () => {
   console.log(`Server listening on ${config.port}`);
 });
 
-socketServer.on('request', req => {
-  const connection = req.accept(config.socketProtocol, req.origin);
+socketServer.on('request', (req: request) => {
+  const connection: connection = req.accept(config.socketProtocol, req.origin);
 
   console.log(`Received incoming connection`);
 
-  connection.on('close', (reason, description) => {
+  connection.on('close', (reason: any, description: any) => {
     console.log(`Connection closed: [${reason}] ${description}`);
     deviceConnection = null;
   });
