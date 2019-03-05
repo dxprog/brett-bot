d71bad842822a7bca249a737773bbdd53d64035d
Matt Hackmann <mhackmann@surveymonkey.com>
2019-03-05 14:50:58 -0800

    scaffolding for soundbite support

--- api-server/index.ts
+++ api-server/index.ts
@@ -1,6 +1,11 @@
 import * as cors from 'cors';
 import * as express from 'express';
+import * as fileUpload from 'express-fileupload';
+import * as fs from 'fs';
+import { UploadedFile } from 'express-fileupload';
 import * as http from 'http';
+import * as mm from 'music-metadata';
+import * as path from 'path';
 import {
   server as WebSocketServer,
   request,
@@ -10,9 +15,18 @@ import {
 import config from '../config';
 import { Logger } from './logger';
 
+const SOUND_BITE_LOCATION = path.resolve('./soundbites');
+const SOUND_BITE_MAX_DURATION = 20;
+const MAX_UPLOAD_SIZE = 2 * 1024 * 1024; // 2MB
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload({
+  // 2MB filesize limit
+  limits: { fileSize: MAX_UPLOAD_SIZE },
+  abortOnLimit: true
+}))
 const httpServer = http.createServer(app);
 const socketServer = new WebSocketServer({
   httpServer,
@@ -48,6 +62,20 @@ app.get('/log', async (req: express.Request, res: express.Response) => {
   const since = req.query.since || Date.now() - (3600 * 12 * 1000);
   const logs = await logger.fetch(since);
   res.json(logs);
+});
+
+app.post('/soundbite', async (req: express.Request, res: express.Response) => {
+  const uploadedFile: UploadedFile = <UploadedFile>req.files.soundUpload;
+  if (uploadedFile) {
+    const id3Tags = await mm.parseBuffer(uploadedFile.data);
+    if (id3Tags && id3Tags.format.duration < SOUND_BITE_MAX_DURATION) {
+
+    } else {
+      res.sendStatus(415);
+    }
+  } else {
+    res.sendStatus(412);
+  }
 });
 
 httpServer.listen(config.port, () => {
--- create-db.ts
+++ create-db.ts
@@ -6,4 +6,5 @@ const db = new sqlite3.Database(config.dbPath);
 
 db.serialize(() => {
   db.run('CREATE TABLE commands (command TEXT, data TEXT, date INT)');
+  db.run('CREATE TABLE sound_bites (title TEXT)');
 });
